592c82e3f616e490151e9095fd15b50c521b831d
sagarsharma21 <007sharma.sagar@gmail.com>
2024-02-22 11:37:26 +0530

    Add Service Layer

---
+++ src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -0,0 +1,26 @@
+package com.telusko.quizapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.telusko.quizapp.Question;
+import com.telusko.quizapp.service.QuestionService;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+	
+	@Autowired
+	QuestionService questionService;
+	
+	@GetMapping("allQuestions")
+
+	public List<Question> getAllQuestions() {
+		return questionService.getAllQuestions(); //"Hi, here are the results!";
+		
+	}
+}
---
+++ src/main/java/com/telusko/quizapp/service/QuestionService.java
@@ -0,0 +1,17 @@
+package com.telusko.quizapp.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.telusko.quizapp.Question;
+
+@Service
+public class QuestionService {
+
+	public List<Question> getAllQuestions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
