7fee5bd78aa210d2213fff5dbd24faabbe620374
sagarsharma21 <007sharma.sagar@gmail.com>
2024-02-22 15:49:30 +0530

    Add DAO Layer

--- src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +21,19 @@ public class QuestionController {
 	QuestionService questionService;
 	
 	@GetMapping("allQuestions")
-
 	public List<Question> getAllQuestions() {
 		return questionService.getAllQuestions(); //"Hi, here are the results!";
 		
 	}
+	
+	@GetMapping("category/{category}")
+	public List<Question> getQuestionsByCategory(@PathVariable String category){
+		return questionService.getQuestionsByCategory(category);
+	}
+	
+	@PostMapping("add")
+	public String addQuestion(@RequestBody Question question) {
+		return questionService.addQuestion(question);
+	}
+	
 }
--- src/main/java/com/telusko/quizapp/service/QuestionService.java
+++ src/main/java/com/telusko/quizapp/service/QuestionService.java
@@ -2,16 +2,33 @@ package com.telusko.quizapp.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.telusko.quizapp.Question;
+import com.telusko.quizapp.dao.QuestionDao;
 
 @Service
 public class QuestionService {
 
+	@Autowired
+	QuestionDao questionDao;
+	
 	public List<Question> getAllQuestions() {
 		// TODO Auto-generated method stub
-		return null;
+		return questionDao.findAll();
+		
+	}
+
+	public List<Question> getQuestionsByCategory(String category) {
+		// TODO Auto-generated method stub
+		return questionDao.findByCategory(category);
+	}
+	
+	public String addQuestion(Question question) {
+		questionDao.save(question);
+		return "Added";
 	}
 
 }
+ 
